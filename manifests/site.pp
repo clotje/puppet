c8129ce10c69cd22df78a31d47af6e42106e6315
clotje <41633130+clotje@users.noreply.github.com>
2022-03-31 10:37:09 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,17 +6,18 @@
 node default {
 file { '/etc/selinux/config':
   ensure => present,
-}->
+}~>
 
 file_line{'Setting SELinux to Permissive mode permanently':
   path   => '/etc/selinux/config',
   line   => 'SELINUX=permissive',
   match  => "^SELINUX=.*$",
-  notify => Exec['noreboot'],
+  notify => Exec["noreboot"],
 }
 
-exec{'noreboot':
+exec{"noreboot":
+  path        => "/sbin:/usr/sbin:/bin:/usr/bin",
+  command     => 'setenforce 0',
   refreshonly => true,
-  command     => '/sbin/setenforce 0',
 }
 }
