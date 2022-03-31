b8d70ed4ec96ff6b69769b6ce5c6f627d97506c5
clotje <41633130+clotje@users.noreply.github.com>
2022-03-31 09:55:15 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,23 @@
-node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
+# @summary LIM RHEL8 SELinux RHEL8 Security
+#
+# Setting SELinux to Permissive mode permanently 
+#
+#
+class security::selinux{
+
+file { '/etc/selinux/config':
+  ensure => present,
+}->
+
+file_line{'Setting SELinux to Permissive mode permanently':
+  path   => '/etc/selinux/config',
+  line   => 'SELINUX=permissive',
+  match  => "^SELINUX=.*$",
+  notify => Exec['noreboot'],
+}
+
+exec{'noreboot':
+  refreshonly => true,
+  command     => 'setenforce 0',
+}
 }
